6fbca9760c9aa3a0b58de2aefed24d2a0ba423cb
alexlbr <alex.lobera@gmail.com>
2018-09-02 13:03:09 +0100

    updates comments src/components/Layout/Avatar.test.js

--- src/components/Layout/Avatar.test.js
+++ src/components/Layout/Avatar.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
-import { shallowToJson } from 'enzyme-to-json'
+import toJson from 'enzyme-to-json'
 import Adapter from 'enzyme-adapter-react-16'
 import Avatar  from './Avatar'
 
@@ -9,6 +9,7 @@ import Avatar  from './Avatar'
 describe('<Avatar />', () => {
   it('renders Avatar', () => {
     // TASK 2: Shallow the Avatar component
+    //    Hint:  https://github.com/adriantoine/enzyme-to-json#helper
     // TASK 3: Create the snapshot
   })
 })
