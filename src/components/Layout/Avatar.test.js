52d34ab48a6fa6e1b6def6b0e28074eea08cb1dc
alexlbr <alex.lobera@gmail.com>
2018-09-02 12:37:07 +0100

    adds Avatar.test.js scaffolding

---
+++ src/components/Layout/Avatar.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+import Adapter from 'enzyme-adapter-react-16'
+import Avatar  from './Avatar'
+
+// TASK 1: configure the Enzyme adapter. Hint https://airbnb.io/enzyme/
+
+describe('<Avatar />', () => {
+  it('renders Avatar', () => {
+    // TASK 2: Shallow the Avatar component
+    // TASK 3: Create the snapshot
+  })
+})
