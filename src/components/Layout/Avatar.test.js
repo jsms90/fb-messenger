0620f7c5372e4a749ee03091cf3ce9416ee94cb2
alexlbr <alex.lobera@gmail.com>
2018-09-02 13:04:06 +0100

    updates src/components/Layout/Avatar.test.js

--- src/components/Layout/Avatar.test.js
+++ src/components/Layout/Avatar.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { shallowToJson } from 'enzyme-to-json'
+import toJson from 'enzyme-to-json'
 import Avatar  from './Avatar'
 
 describe('<Avatar />', () => {
   it('renders Avatar', () => {
     const wrapper = shallow(<Avatar />)
-    expect(shallowToJson(wrapper)).toMatchSnapshot()
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
