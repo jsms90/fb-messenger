efc1ad7b7ffe461f4bc05f037a1220417f430e35
alexlbr <alex.lobera@gmail.com>
2018-10-03 20:02:11 +0100

    adds flckr proxy api

--- api/index.js
+++ api/index.js
@@ -3,15 +3,27 @@ const express = require('express');
 
 const app = express();
 
+global.photos = []
+
 const getPhotosFromFlckr = async () => {
     const reqId = '1'
-    const response = await fetch(`https://api.flickr.com/v2/media/search?reqId=${reqId}&orderBy=interestingness&query=flicker&pageNumber=3&pageSize=50&format=json&nojsoncallback=1`)
+    const pageNumber = 1
+    const response = await fetch(`https://api.flickr.com/v2/media/search?reqId=${reqId}&orderBy=interestingness&query=flicker&pageNumber=${pageNumber}&pageSize=50&format=json&nojsoncallback=1`)
+    const data = await response.json()
 
-    return response.json()
+    global.photos = [...global.photos, ...data]
+
+    return data
 }
 
-app.get('/photos', (req, res) => {
-    res.json(getPhotosFromFlckr())
+app.get('/photos', async (req, res) => {
+    const data = await getPhotosFromFlckr()
+    res.json(data)
+})
+
+app.get('/photo/:id', (req, res) => {
+    const photo = global.photos.find(photo => photo.id === req.params.id)
+    res.json(photo)
 })
 
 const server = app.listen(process.env.PORT || 8888, () => {
