e174f15f3acbfae2ba6a0dc6e8ff90d4ac517f57
Alex Lobera <alex.lobera@gmail.com>
2018-11-06 10:12:37 -0500

    Update ui.test.js

--- src/reducers/ui.test.js
+++ src/reducers/ui.test.js
@@ -10,7 +10,7 @@ describe('UI reducer', () => {
     const actualState = uiReducer(undefined, { type: "NO_RELEVANT_ACTION" })
     const expectedState = getInitialState()
 
-    expect(expectedState).toEqual(actualState)
+    expect(actualState).toEqual(expectedState)
   })
 
   it(`should return a new state if it receives a ${TOGGLE_MESSAGE_DETAIL} action`, () => {
@@ -18,7 +18,7 @@ describe('UI reducer', () => {
     const expectedState = getInitialState()
     expectedState.isMessageDetailOpen = true
 
-    expect(expectedState).toEqual(actualState)
+    expect(actualState).toEqual(expectedState)
   })
 
   it(`should return the current state if the action it receives it's not handled by the reducer`, () => {
@@ -26,6 +26,6 @@ describe('UI reducer', () => {
     expectedState.isMessageDetailOpen = true
     const actualState = uiReducer(expectedState, { type: "NO_RELEVANT_ACTION" })
 
-    expect(expectedState).toEqual(actualState)
+    expect(actualState).toEqual(expectedState)
   })
 })
