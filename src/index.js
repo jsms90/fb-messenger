ca9c6c0b6b419022c0e9188b29db3c10468bfcd2
Horacio Herrera <hherrerag@me.com>
2018-11-12 22:49:57 +0000

    fix components path on Root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Root from './Components/Root'
+import Root from './components/Root'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
