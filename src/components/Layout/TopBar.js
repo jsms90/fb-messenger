05434bcfe93ebb5570fab38170705678179fde50
Jennifer Spencer <jennifer.spencer@yld.io>
2018-12-03 17:47:46 +0000

    add /profile to routing

--- src/components/Layout/TopBar.js
+++ src/components/Layout/TopBar.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const TopBar = (props) => (
+const TopBar = props => (
   <div className="top-bar">
-    <Link to="/messages"><i className="icon fab fa-facebook-messenger" /></Link>
-    <Link to="?" className={`username ${props.userPosition || 'right'}`}>
-     profile <img src="/images/default.jpg" />
+    <Link to="/messages">
+      <i className="icon fab fa-facebook-messenger" />
+    </Link>
+    <Link to="/profile" className={`username ${props.userPosition || "right"}`}>
+      profile <img src="/images/default.jpg" />
     </Link>
   </div>
-)
+);
 
-export default TopBar
+export default TopBar;
