6a4d876fabc24dd9b659131dbaac6fc5ef0ac3ce
Jennifer Spencer <jennifer.spencer@yld.io>
2018-12-03 18:07:09 +0000

    refacotor Threads component to use container pattern

--- src/components/Messenger/ThreadsContainer.js
+++ src/components/Messenger/ThreadsContainer.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router'
+import React, { Component } from "react";
+import { withRouter } from "react-router";
 
-import Threads from './Threads'
-import { fetchThreads } from '../../api/thread'
+import Threads from "./Threads";
+import { fetchThreads } from "../../api/thread";
 
 class ThreadsContainer extends Component {
   constructor(props) {
-    super(props)
-
-    // hint, add some state here
+    super(props);
+    this.state = {
+      threads: []
+    };
   }
 
   componentDidMount() {
-    // hint, you should fetch the threads here
+    fetchThreads()
+      .then(({ threads }) => {
+        this.setState({ threads });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
-    const { showSettings, newMessage, history, match } = this.props
-    const { threads } = this.state
+    const { showSettings, newMessage, history, match } = this.props;
+    const { threads } = this.state;
 
     return (
-      // hint, which component and props do you think we should return here?
-    )
+      <Threads
+        showSettings={showSettings}
+        newMessage={newMessage}
+        history={history}
+        match={match}
+        threads={threads}
+      />
+    );
   }
 }
 
-export default withRouter(ThreadsContainer)
+export default withRouter(ThreadsContainer);
--- src/components/Messenger/index.js
+++ src/components/Messenger/index.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Route } from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { Route } from "react-router-dom";
 
-import Threads from './Threads'
-import Conversation from './Conversation/Conversation'
+import ThreadsContainer from "./ThreadsContainer";
+import Conversation from "./Conversation/Conversation";
 
-const Messenger = ({ showSettings, toggleModal }) => (
+const Messenger = ({
+  showSettings,
+  newMessage,
+  toggleModal,
+  history,
+  match
+}) => (
   <div className="messenger">
-    <Threads />
+    <ThreadsContainer
+      className="messenger"
+      showSettings={showSettings}
+      newMessage={newMessage}
+      history={history}
+      match={match}
+    />
     <Route path={`/messages/:username`} component={Conversation} />
   </div>
-)
+);
 
 Messenger.propTypes = {
   showSettings: PropTypes.func,
-  newMessage: PropTypes.func,
-}
+  newMessage: PropTypes.func
+};
 
-export default Messenger
+export default Messenger;
