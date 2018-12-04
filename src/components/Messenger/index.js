adbd428c67fc44a992c0ff30081b89fdfa9561ad
Jennifer Spencer <jennifer.spencer@yld.io>
2018-12-04 09:31:50 +0000

    remove unused toggleModal prop from Messenger component

--- src/components/Messenger/index.js
+++ src/components/Messenger/index.js
@@ -5,13 +5,7 @@ import { Route } from "react-router-dom";
 import ThreadsContainer from "./ThreadsContainer";
 import ConversationContainer from "./Conversation/ConversationContainer";
 
-const Messenger = ({
-  showSettings,
-  newMessage,
-  toggleModal,
-  history,
-  match
-}) => (
+const Messenger = ({ showSettings, newMessage, history, match }) => (
   <div className="messenger">
     <ThreadsContainer
       className="messenger"
