042859b414ad6295836b480c066cf6667cc28510
Jennifer Spencer <jennifer.spencer@yld.io>
2018-12-04 12:33:44 +0000

    part 1 of exercise: login form make email & password inputs controlled add client side validation redirect to homepage if login is successful

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,50 +1,65 @@
-import React, { Component } from 'react'
-import './Login.css'
+import React, { Component } from "react";
+import "./Login.css";
 
 class Login extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     // Here you'll need to set up the state of the form
-    this.state = {}
+    this.state = {
+      email: "",
+      emailHasError: false,
+      password: "",
+      passwordError: false,
+      submissionHasError: false
+    };
   }
 
   handleChange = (name, event) => {
-    let change = {}
-    /*
-      explanation about the expression: change[name]
-      change.email = event  // the change variable is an object and so you can add a property by doing .name_of_the_property
-      change["email"] = event // the change variable is an object and so you can add a property by doing ["string_with_the_name_of_the_property_notice_the_quotes"]
-      change[name] = event // the change variable is an object and so you can add a property by doing [variable_with_the_name_of_the_property_notice_no_quotes]
-    */
-    change[name] = event.target.value
-    //You need to set the change object in the state of the component
-  }
+    this.setState({
+      [name]: event.target.value
+    });
+  };
 
-  handleSubmit = async (e) => {
-    // When the button(which type=submit) is clicked, we can stop the form submission by doing:
-    e.preventDefault()
+  handleSubmit = async e => {
+    e.preventDefault();
 
-    const { history } = this.props
-    const { password, email } = this.state
+    const { history } = this.props;
+    const { password, email } = this.state;
 
-    // You can add some validation here to make sure password and email are provided
-
-    const { status } = await fetch('/api/auth', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include',
-      body: JSON.stringify({ password, email })
-    })
-
-    if (status === 200) {
-      // Here you need to "send" the user to '/'
+    if (!this.state.email) {
+      this.setState({ emailHasError: true });
     }
-  }
+    if (!this.state.password) {
+      this.setState({ passwordHasError: true });
+    } else {
+      const { status } = await fetch("/api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include",
+        body: JSON.stringify({ password, email })
+      });
 
-  render () {
+      console.log("status", status);
+      if (status === 200) {
+        history.push("/");
+      } else {
+        this.setState({ submissionHasError: true });
+      }
+    }
+  };
+
+  render() {
+    const {
+      email,
+      emailHasError,
+      password,
+      passwordHasError,
+      submissionHasError
+    } = this.state;
+
     return (
       <form className="form-signin">
         <div className="form-group">
@@ -55,15 +70,25 @@ class Login extends Component {
             type="email"
             placeholder="Enter email"
             className="form-control"
-            value={this.state.email}
-            onChange={e => this.handleChange('email', e)}
+            value={email}
+            onChange={e => this.handleChange("email", e)}
           />
+          {emailHasError && !submissionHasError && (
+            <p style={{ color: "red" }}>Oi! Pay attention to me!</p>
+          )}
           <input
             type="password"
             placeholder="Enter password"
             className="form-control"
-            value={this.state.password}
+            value={password}
+            onChange={e => this.handleChange("password", e)}
           />
+          {passwordHasError && !submissionHasError && (
+            <p style={{ color: "red" }}>Oi! Pay attention to me!</p>
+          )}
+          {submissionHasError && (
+            <p style={{ color: "red" }}>Yo that's wrong!</p>
+          )}
         </div>
         <button
           type="submit"
@@ -73,8 +98,8 @@ class Login extends Component {
           Sign in
         </button>
       </form>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
