bb0c164c40442b30e28a92e3e321eca1628e5b08
Jennifer Spencer <jennifer.spencer@yld.io>
2018-12-04 10:07:29 +0000

    make login page function correctly

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,46 +1,77 @@
-import React, {Component} from 'react'
-import {FormGroup, FormControl, Button} from 'react-bootstrap'
-import './Login.css'
+import React, { Component } from "react";
+import { FormGroup, FormControl, Button } from "react-bootstrap";
+import "./Login.css";
 
 class Login extends Component {
-    constructor() {
-        super()
-        //Here you'll need to set up the state of the component
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // Hint: In order for this to work you'll need to use the withRouter HOC:
+    // https://reacttraining.com/react-router/web/api/withRouter
+    const { history } = this.props;
+
+    if (this.state.username === "react" && this.state.password === "1234") {
+      history.push("/");
     }
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        // Hint: In order for this to work you'll need to use the withRouter HOC:
-        // https://reacttraining.com/react-router/web/api/withRouter
-        const { history } = this.props
-
-        if (this.state.username === 'react' && this.state.password === '1234') {
-            history.push('/')
-        }
+  handleChange = (name, event) => {
+    if (name === "email") {
+      this.setState({ username: event.target.value });
+    } else if ((name = "password")) {
+      this.setState({ password: event.target.value });
     }
+    // let change = {};
+    // change[name] = event.target.value;
+    //You need to set the change object in the state of the component
+  };
 
-    handleChange = (name, event) => {
-        let change = {}
-        change[name] = event.target.value
-        //You need to set the change object in the state of the component
-    }
+  render() {
+    const { username, password } = this.state;
+    return (
+      <form className="form-signin">
+        <FormGroup>
+          <h2 className="form-signin-heading">Please sign in</h2>
+        </FormGroup>
 
-    render () {
-        return (
-            <form className="form-signin">
-                <FormGroup>
-                    <h2 className="form-signin-heading">Please sign in</h2>
-                </FormGroup>
+        <FormGroup>
+          <FormControl
+            onChange={event => this.handleChange("email", event)}
+            className="form-control"
+            id="email"
+            type="email"
+            value={username}
+            placeholder="Enter email"
+          />
+          <FormControl
+            onChange={event => this.handleChange("password", event)}
+            className="form-control"
+            id="password"
+            type="password"
+            value={password}
+            placeholder="Password"
+          />
+        </FormGroup>
 
-                <FormGroup>
-                    <FormControl onChange={(event) => this.handleChange('email', event)} className="form-control" id="email" type="email" value="" placeholder="Enter email" />
-                    <FormControl className="form-control" id="password" type="password" value="" placeholder="Password" />
-                </FormGroup>
-
-                <Button bsSize="large" bsStyle="primary" block type="submit">Sign in</Button>
-            </form>
-        )
-    }
+        <Button
+          bsSize="large"
+          bsStyle="primary"
+          block
+          type="submit"
+          onClick={event => this.handleSubmit(event)}
+        >
+          Sign in
+        </Button>
+      </form>
+    );
+  }
 }
 
-export default Login
+export default Login;
